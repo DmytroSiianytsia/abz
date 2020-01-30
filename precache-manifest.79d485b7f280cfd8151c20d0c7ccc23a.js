0c7c8bb922b2bcd68be1e24d02a3c63026382e36
Dima <ds081181@gmail.com>
2020-01-30 16:19:16 +0200

    Updates

---
+++ precache-manifest.79d485b7f280cfd8151c20d0c7ccc23a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4a2882dd77ccc5168fc04677defb4d",
+    "url": "/abz/index.html"
+  },
+  {
+    "revision": "b7c929e39212a60a77d4",
+    "url": "/abz/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b839e7d4f64e3ff0a71d",
+    "url": "/abz/static/js/2.fe331eed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/abz/static/js/2.fe331eed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7c929e39212a60a77d4",
+    "url": "/abz/static/js/main.fc0d1668.chunk.js"
+  },
+  {
+    "revision": "61e54c6be3ec5341e84e",
+    "url": "/abz/static/js/runtime-main.ef00a0b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/abz/static/media/logo.5d5d9eef.svg"
+  }
+]);

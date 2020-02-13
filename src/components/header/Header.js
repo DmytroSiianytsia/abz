5cac8f0182ad425c009a09bbd1b8ab1922439aef
Dima <ds081181@gmail.com>
2020-02-13 22:34:07 +0200

    add aside menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
-function Header({ scrollTo }) { 
+function Header({ scrollTo }) {
+    const [isVisible, toggleVisible] = useState(false);  
+    const navAside = isVisible ? 'nav-aside' : 'nav-aside visible'; 
     return (
         <div className="header">
             <div className="container">
                 <div className="header__content">
                     <img src="./img/logo.svg" alt="test task" className="logo__img" />
-                    <img src="./img/menu-icon.svg" alt="menu" className="header__menu-burger" />                    
+                    <img
+                        src="./img/menu-icon.svg"
+                        alt="menu"
+                        className="header__menu-burger"
+                        onClick={() => toggleVisible(!isVisible)}
+                    />
                     <nav className="nav-header">
                         <ul className="nav">
                             <li className="nav__item" tabIndex="0" onClick={() => scrollTo(0)}>Main</li>
-                            <li className="nav__item" tabIndex="1" onClick={() => scrollTo(650)}>About me </li>                            
+                            <li className="nav__item" tabIndex="1" onClick={() => scrollTo(650)}>About me </li>
                             <li className="nav__item" tabIndex="2">Requirements</li>
                             <li className="nav__item" tabIndex="3" onClick={() => scrollTo(1400)}>Users</li>
                             <li className="nav__item" tabIndex="4" onClick={() => scrollTo(2555)}>Sign Up</li>
+                        </ul>
+                    </nav>
+                    <nav className={navAside}>
+                        <div className="nav-aside__logo">
+                            <img src="./img/logo.svg" alt="test task" className="logo__img" />
+                        </div>
+                        <hr className="hor-line" />
+                        <ul className="nav1">
+                            <li className="nav__item" tabIndex="0" onClick={() => scrollTo(0)}>Main</li>
+                            <li className="nav__item" tabIndex="1" onClick={() => scrollTo(650)}>About me </li>
+                            <li className="nav__item" tabIndex="2">Requirements</li>
+                            <li className="nav__item" tabIndex="3" onClick={() => scrollTo(1400)}>Users</li>
+                            <li className="nav__item" tabIndex="4" onClick={() => scrollTo(2555)}>Sign Up</li>
+                        </ul>
+                        <hr />
+                        <ul className="nav1">
+                            <li className="nav__item" tabIndex="5">How it works</li>
+                            <li className="nav__item" tabIndex="6">Partnership</li>
+                            <li className="nav__item" tabIndex="7">Help</li>
+                            <li className="nav__item" tabIndex="8">Leave testimonial</li>
+                            <li className="nav__item" tabIndex="9">Contact us</li>
+                        </ul>
+                        <hr />
+                        <ul className="nav1">
+                            <li className="nav__item" tabIndex="10">Articles</li>
+                            <li className="nav__item" tabIndex="11">Our news</li>
+                            <li className="nav__item" tabIndex="12">Testimonials</li>
+                            <li className="nav__item" tabIndex="13">Licenses</li>
+                            <li className="nav__item" tabIndex="14">Privacy Policy</li>
                         </ul>
                     </nav>
                 </div>
